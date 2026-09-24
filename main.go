4f26037f398c32ca3fe96218ed159c082decc657
Izenberk <aceskorn555@outlook.com>
2026-09-24 10:13:30 +0700

    feat: add cors and set allow all

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/Izenberk/url-shortener-api/api/routes"
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/logger"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -27,6 +28,7 @@ func main() {
 
 	app := fiber.New()
 	app.Use(logger.New())
+	app.Use(cors.New())
 
 	setupRoutes(app)
 
